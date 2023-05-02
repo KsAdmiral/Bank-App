1896782eed7ae44a381f058d7b1348a4d9248c1d
KsAdmiral <mkilnivz@gmail.com>
2023-05-02 14:46:37 +0300

    fixes

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ app.use("/users", userRoutes);
 
 //static production asset
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/build")));
-  // app.use(express.static(path.join(__dirname, "frontend", "build")));
+  app.use(express.static(path.join(__dirname, "bank-frontend", "build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-    // res.sendFile(path.resolve(__dirname,'./fmh-client/build','index.html'))
+    res.sendFile(
+      path.resolve(__dirname, "bank-frontend", "build", "index.html")
+    );
   });
 }
 
